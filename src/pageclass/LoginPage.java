d430197648c075ec6fece4f3d9d2d21020ba123c
dikshamtesgithub <119382373+dikshamtesgithub@users.noreply.github.com>
2023-07-04 02:02:27 -0700

    commiting login test

---
+++ src/pageclass/LoginPage.java
@@ -0,0 +1,10 @@
+package pageclass;
+
+public class LoginPage {
+	
+	public void validate_login()
+	{
+		System.out.println("loginpage  to app ");
+	}
+
+}
---
+++ src/testclss/LoginTest.java
@@ -0,0 +1,10 @@
+package testclss;
+
+public class LoginTest {
+	
+	public  void verify_login()
+	{
+		System.out.println("logii to app test");
+	}
+
+}
